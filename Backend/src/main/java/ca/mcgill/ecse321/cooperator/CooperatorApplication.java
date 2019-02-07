c3f8ef87933ebaa82a388472d7fd9d6f987f11b6
fangnx <fangnaxin@gmail.com>
2019-02-07 17:14:01 -0500

    Test Travis CI

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/CooperatorApplication.java
@@ -1,9 +1,10 @@
 package ca.mcgill.ecse321.cooperator;
-//test travis
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+//Test Travis CI integration
 
 @RestController
 @SpringBootApplication
