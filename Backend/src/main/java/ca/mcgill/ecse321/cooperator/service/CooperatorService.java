16dee0ff36b8b28b328a0285468d1675ac5e1cea
unknown <remi.carriere@mail.mcgill.ca>
2019-02-07 16:01:44 -0500

    Added exceptions for writing null objects to DB and relevant test cases

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/service/CooperatorService.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/service/CooperatorService.java
@@ -1,11 +1,12 @@
 package ca.mcgill.ecse321.cooperator.service;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import ca.mcgill.ecse321.cooperator.model.CoopCourse;
 import ca.mcgill.ecse321.cooperator.model.Task;
@@ -41,151 +42,283 @@ public class CooperatorService {
 	TaskRepository taskRepository;
 	@Autowired
 	DocumentRepository documentRepository;
-	
+
 	/*--- STUDENT METHODS ---*/
-    @Transactional
-    public Student createStudent(String firstName, String lastName, Integer id, String email) {
-        Student s = new Student();
-        s.setFirstName(firstName);
-        s.setLastName(lastName);
-        s.setMcgillID(id);
-        s.setMcgillEmail(email);
-        studentRepository.save(s);
-        return s;
-    }
-    
-    @Transactional
-    public Student getStudent(Integer id) {
-        Student s = studentRepository.findStudentByMcgillID(id);
-        return s;
-    }
-    
-    /*--- EMPLOYER METHODS ---*/
-    @Transactional
-    public Employer createEmployer(String name, String email) {
-        Employer e = new Employer();
-        e.setName(name);
-        e.setEmail(email);
-        employerRepository.save(e);
-        return e;
-    }
+	@Transactional
+	public Student createStudent(String firstName, String lastName, Integer id, String email) {
+		if (incorrectStudentDetails(firstName, lastName, id, email)) {
+			throw new IllegalArgumentException("Your student details are incomplete!");
+		}
+		Student s = new Student();
+		s.setFirstName(firstName);
+		s.setLastName(lastName);
+		s.setMcgillID(id);
+		s.setMcgillEmail(email);
+		studentRepository.save(s);
+		return s;
+	}
 
-    @Transactional
-    public Employer getEmployer(String email) {
-        Employer e = employerRepository.findEmloyerByEmail(email);
-        return e;
-    }
-    
-    /*--- COOP COURSE METHODS ---*/
-    @Transactional
-    public CoopCourse createCoopCourse(String courseCode, Integer coopTerm) {
-        CoopCourse c = new CoopCourse();
-        c.setCourseCode(courseCode);
-        c.setCoopTerm(coopTerm);
-        coopCourseRepository.save(c);
-        return c;
-    }
+	@Transactional
+	public Student getStudent(Integer id) {
+		Student s = studentRepository.findStudentByMcgillID(id);
+		return s;
+	}
 
-    @Transactional
-    public CoopCourse getCoopCourse(String coopCourseID) {
-        CoopCourse c  = coopCourseRepository.findCoopCourseByCourseCode(coopCourseID);
-        return c;
-    }
+	@Transactional
+	public List<Student> getAllStudents() {
+		return toList(studentRepository.findAll());
+	}
 
-    /*--- COOP COURSE OFFERING METHODS ---*/
-    @Transactional
-    public CoopCourseOffering createCoopCourseOffering(Integer year, Term term, Boolean active, CoopCourse coopCourse) {
-        CoopCourseOffering cco = new CoopCourseOffering();
-        cco.setYear(year);
-        cco.setTerm(term);
-        cco.setActive(active);
-        cco.setCoopCourse(coopCourse);
-        String offerID = coopCourse.getCourseCode();
-        switch(term) {
-            case FALL:
-                offerID += "-F";
-                break;
-            case WINTER:
-                offerID += "-W";
-                break;
-            case SUMMER:
-                offerID += "-S";
-                break;
-        }
-            offerID += year % 2000;
-            cco.setOfferID(offerID);
-            coopCourseOfferingRepository.save(cco);
-            return cco;
-    }
+	private boolean incorrectStudentDetails(String firstName, String lastName, Integer id, String email) {
+		if (firstName == null || firstName.trim().length() == 0) {
+			return true;
+		}
+		if (lastName == null || lastName.trim().length() == 0) {
+			return true;
+		}
+		if (id == null) {
+			return true;
+		}
+		if (email == null || email.trim().length() == 0) {
+			return true;
+		}
+		return false;
+	}
 
-    @Transactional
-    public CoopCourseOffering getCoopCourseOffering(String offerID) {
-        CoopCourseOffering cco = coopCourseOfferingRepository.findCoopCourseOfferingByOfferID(offerID);
-        return cco;
-    }
-    
-    /*--- STUDENT ENROLLMENT METHODS ---*/
-    @Transactional
-    public StudentEnrollment createStudentEnrollment(Boolean active, CourseStatus status, Student s, Employer e, CoopCourseOffering cco) {
-        StudentEnrollment se = new StudentEnrollment();
-        se.setEnrolledStudent(s);
-        se.setStudentEmployer(e);
-        se.setActive(active);
-        se.setStatus(status);
-        se.setEnrollmentID(s.getMcgillID() + "-" + cco.getOfferID());
-        se.setCoopCourseOffering(cco);
+	/*--- EMPLOYER METHODS ---*/
+	@Transactional
+	public Employer createEmployer(String name, String email) {
+		if (incorrectEmployerDetails(name, email)) {
+			throw new IllegalArgumentException("Your employer details are incomplete!");
+		}
+		Employer e = new Employer();
+		e.setName(name);
+		e.setEmail(email);
+		employerRepository.save(e);
+		return e;
+	}
 
-        cco.addStudentEnrollment(se);
+	@Transactional
+	public Employer getEmployer(String email) {
+		Employer e = employerRepository.findEmloyerByEmail(email);
+		return e;
+	}
 
-        studentEnrollmentRepository.save(se);
-        return se;
-    }
+	@Transactional
+	public List<Employer> getAllEmployers() {
+		return toList(employerRepository.findAll());
+	}
 
-    @Transactional
-    public StudentEnrollment getStudentEnrollment(String id) {
-        StudentEnrollment se = studentEnrollmentRepository.findStudentEnrollmentByEnrollmentID(id);
-        return se;
-    }
+	private boolean incorrectEmployerDetails(String name, String email) {
+		if (name == null || name.trim().length() == 0) {
+			return true;
+		}
+		// TODO Other Cases
+		return false;
+	}
+
+	/*--- COOP COURSE METHODS ---*/
+	@Transactional
+	public CoopCourse createCoopCourse(String courseCode, Integer coopTerm) {
+		if (incorrectCourseDetails(courseCode, coopTerm)) {
+			throw new IllegalArgumentException("Your course details are incomplete!");
+		}
+		CoopCourse c = new CoopCourse();
+		c.setCourseCode(courseCode);
+		c.setCoopTerm(coopTerm);
+		coopCourseRepository.save(c);
+		return c;
+	}
+
+	@Transactional
+	public CoopCourse getCoopCourse(String coopCourseID) {
+		CoopCourse c = coopCourseRepository.findCoopCourseByCourseCode(coopCourseID);
+		return c;
+	}
+
+	@Transactional
+	public List<CoopCourse> getAllCoopCourses() {
+		return toList(coopCourseRepository.findAll());
+	}
+
+	private boolean incorrectCourseDetails(String courseCode, Integer coopTerm) {
+		if (courseCode == null || courseCode.trim().length() == 0) {
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*--- COOP COURSE OFFERING METHODS ---*/
+	@Transactional
+	public CoopCourseOffering createCoopCourseOffering(Integer year, Term term, Boolean active, CoopCourse coopCourse) {
+		if (incorrectCourseOfferingDetails(year, term, active, coopCourse)) {
+			throw new IllegalArgumentException("Your course offering details are incomplete!");
+		}
+		CoopCourseOffering cco = new CoopCourseOffering();
+		cco.setYear(year);
+		cco.setTerm(term);
+		cco.setActive(active);
+		cco.setCoopCourse(coopCourse);
+		String offerID = coopCourse.getCourseCode();
+		switch (term) {
+		case FALL:
+			offerID += "-F";
+			break;
+		case WINTER:
+			offerID += "-W";
+			break;
+		case SUMMER:
+			offerID += "-S";
+			break;
+		}
+		offerID += year % 2000;
+		cco.setOfferID(offerID);
+		coopCourseOfferingRepository.save(cco);
+		return cco;
+	}
+
+	@Transactional
+	public CoopCourseOffering getCoopCourseOffering(String offerID) {
+		CoopCourseOffering cco = coopCourseOfferingRepository.findCoopCourseOfferingByOfferID(offerID);
+		return cco;
+	}
+
+	@Transactional
+	public List<CoopCourseOffering> getAllCoopCourseOfferings() {
+		return toList(coopCourseOfferingRepository.findAll());
+	}
+
+	private boolean incorrectCourseOfferingDetails(Integer year, Term term, Boolean active, CoopCourse coopCourse) {
+		if (year == null) {
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*--- STUDENT ENROLLMENT METHODS ---*/
+	@Transactional
+	public StudentEnrollment createStudentEnrollment(Boolean active, CourseStatus status, Student s, Employer e,
+			CoopCourseOffering cco) {
+		if (incorrectStudentEnrollmentDetails(active, status, s, e, cco)) {
+			throw new IllegalArgumentException("Your student enrollment details are incomplete!");
+		}
+		StudentEnrollment se = new StudentEnrollment();
+		se.setEnrolledStudent(s);
+		se.setStudentEmployer(e);
+		se.setActive(active);
+		se.setStatus(status);
+		se.setEnrollmentID(s.getMcgillID() + "-" + cco.getOfferID());
+		se.setCoopCourseOffering(cco);
+
+		cco.addStudentEnrollment(se);
+
+		studentEnrollmentRepository.save(se);
+		return se;
+	}
+
+	@Transactional
+	public StudentEnrollment getStudentEnrollment(String id) {
+		StudentEnrollment se = studentEnrollmentRepository.findStudentEnrollmentByEnrollmentID(id);
+		return se;
+	}
+
+	@Transactional
+	public List<StudentEnrollment> getAllStudentEnrollments() {
+		return toList(studentEnrollmentRepository.findAll());
+	}
+
+	private boolean incorrectStudentEnrollmentDetails(Boolean active, CourseStatus status, Student s, Employer e,
+			CoopCourseOffering cco) {
+		if (active == null) {
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*--- TASK METHODS ---*/
+	@Transactional
+	public Task createTask(String description, Date dueDate, TaskStatus status, String taskID, StudentEnrollment se) {
+		if (incorrectTaskDetails(description, dueDate, status, taskID, se)) {
+			throw new IllegalArgumentException("Your task details are incomplete!");
+		}
+		Task t = new Task();
+		t.setDescription(description);
+		t.setDueDate(dueDate);
+		t.setTaskStatus(status);
+		t.setTaskID(taskID);
+		t.setStudentEnrollment(se);
+
+		se.addCourseTask(t);
+
+		taskRepository.save(t);
+		return t;
+	}
+
+	@Transactional
+	public Task getTask(String taskID) {
+		Task t = taskRepository.findTaskByTaskID(taskID);
+		return t;
+	}
+
+	@Transactional
+	public List<Task> getAllTasks() {
+		return toList(taskRepository.findAll());
+	}
+
+	private boolean incorrectTaskDetails(String description, Date dueDate, TaskStatus status, String taskID,
+			StudentEnrollment se) {
+		if (description == null || description.trim().length() == 0) {
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*--- DOCUMENT METHODS ---*/
+	@Transactional
+	public Document createDocument(String name, String url, Task t) {
+		if (incorrectDocumentDetails(name, url, t)) {
+			throw new IllegalArgumentException("Your document details are incomplete!");
+		}
+		Document d = new Document();
+		d.setName(name);
+		d.setUrl(url);
+		d.setTask(t);
+
+		t.addDocument(d);
+		documentRepository.save(d);
+		return d;
+	}
+
+	@Transactional
+	public Document getDocument(String url) {
+		Document doc = documentRepository.findDocumentByUrl(url);
+		return doc;
+	}
+
+	@Transactional
+	public List<Document> getAllDocuments() {
+		return toList(documentRepository.findAll());
+	}
 	
-    /*--- TASK METHODS ---*/
-    @Transactional
-    public Task createTask(String description, Date dueDate, TaskStatus status, String taskID, StudentEnrollment se) {
-        Task t = new Task();
-        t.setDescription(description);
-        t.setDueDate(dueDate);
-        t.setTaskStatus(status);
-        t.setTaskID(taskID);
-        t.setStudentEnrollment(se);
+	private boolean incorrectDocumentDetails(String name, String url, Task t) {
+		if (name == null|| name.trim().length() == 0) {
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-        se.addCourseTask(t);
-
-        taskRepository.save(t);
-        return t;
-    }
-
-    @Transactional
-    public Task getTask(String taskID) {
-        Task t  = taskRepository.findTaskByTaskID(taskID);
-        return t;
-    }
-    
-    /*--- DOCUMENT METHODS ---*/
-    @Transactional
-    public Document createDocument(String name, String url, Task t) {
-        Document d = new Document();
-        d.setName(name);
-        d.setUrl(url);
-        d.setTask(t);
-
-        t.addDocument(d);
-        documentRepository.save(d);
-        return d;
-    }
-
-    @Transactional
-    public Document getDocument(String url) {
-        Document doc = documentRepository.findDocumentByUrl(url);
-        return doc;
-    }
+	/*--- UTILITY METHODS ---*/
+	private <T> List<T> toList(Iterable<T> iterable) {
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
 
 }
--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestCooperatorService.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestCooperatorService.java
@@ -1,10 +1,13 @@
 package ca.mcgill.ecse321.cooperator.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,10 +41,10 @@ public class TestCooperatorService {
 
 	@Autowired
 	private StudentRepository studentRepository;
-	
+
 	@Autowired
 	private EmployerRepository employerRepository;
-	
+
 	@Autowired
 	private CoopCourseRepository coopCourseRepository;
 	@Autowired
@@ -52,8 +55,8 @@ public class TestCooperatorService {
 	private TaskRepository taskRepository;
 	@Autowired
 	private DocumentRepository documentRepository;
-	
-	@After
+
+	@Before
 	public void cleanDataBase() {
 		documentRepository.deleteAll();
 		taskRepository.deleteAll();
@@ -64,104 +67,312 @@ public class TestCooperatorService {
 		coopCourseRepository.deleteAll();
 	}
 
+	/*--- STUDENT TESTS ---*/
 	@Test
 	public void testCreateStudent() {
-		service.createStudent("first_name", "last_name", 260112233, "student@email.com");
-
+		try {
+			service.createStudent("first_name", "last_name", 260112233, "student@email.com");
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
 		Student s = service.getStudent(260112233);
 
 		assertEquals("first_name", s.getFirstName());
 		assertEquals("last_name", s.getLastName());
 		assertEquals((Integer) 260112233, s.getMcgillID());
 		assertEquals("student@email.com", s.getMcgillEmail());
+
+		assertEquals(1, service.getAllStudents().size());
+		assertEquals((Integer) 260112233, service.getAllStudents().get(0).getMcgillID()); // probably not necessary
 	}
-	
-    @Test
-    public void testCreateEmployer() {
-        service.createEmployer("Google", "google@gmail.com");
 
-        Employer e = service.getEmployer("google@gmail.com");
+	@Test
+	public void testCreateNullNameStudent() {
+		String error = null;
+		try {
+			service.createStudent(null, "last_name", 260112233, "student@email.com");
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
 
-        assertEquals("Google", e.getName());
-        assertEquals("google@gmail.com", e.getEmail());
-    }
-    
-    @Test
-    public void testCreateCoopCourse() {
-        service.createCoopCourse("ECSE300", 1);
-        CoopCourse c = coopCourseRepository.findCoopCourseByCourseCode("ECSE300");
+		assertEquals("Your student details are incomplete!", error);
 
-        assertEquals("ECSE300", c.getCourseCode());
-        assertEquals((Integer)1, c.getCoopTerm());
-    }
-    
-    @Test
-    public void testCreateCoopCourseOffering() {
-        CoopCourse c = service.createCoopCourse("ECSE301", 1);
+		assertEquals(0, service.getAllStudents().size());
+	}
 
-        service.createCoopCourseOffering(2018, Term.WINTER, true, c);
+	@Test
+	public void testCreateEmptyNameStudent() {
+		String error = null;
+		try {
+			service.createStudent(" ", "last_name", 260112233, "student@email.com");
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
 
-        CoopCourseOffering cco = coopCourseOfferingRepository.findCoopCourseOfferingByOfferID("ECSE301-W18");
+		assertEquals("Your student details are incomplete!", error);
 
-        assertEquals((Integer)2018, cco.getYear());
-        assertEquals(Term.WINTER, cco.getTerm());
-        assertEquals(true, cco.getActive());
-        assertEquals("ECSE301-W18", cco.getOfferID());
-    }
-    @Test
-    public void testCreateStudentEnrollment() {
-        CoopCourse c = service.createCoopCourse("ECSE302", 1);
-        CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
-        Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
-        Employer e = service.createEmployer("Facebook", "fb@email.com");
+		assertEquals(0, service.getAllStudents().size());
+	}
 
-        service.createStudentEnrollment(true, CourseStatus.PASSED,s, e, cco);
+	@Test
+	public void testCreateNullIDStudent() {
+		String error = null;
+		try {
+			service.createStudent("first_name", "last_name", null, "student@email.com");
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
 
-        StudentEnrollment se = studentEnrollmentRepository.findStudentEnrollmentByEnrollmentID("260654321-ECSE302-F19");
+		assertEquals("Your student details are incomplete!", error);
 
-        assertEquals(true, se.getActive());
-        assertEquals("260654321-ECSE302-F19", se.getEnrollmentID());
-        assertEquals(CourseStatus.PASSED, se.getStatus());
+		assertEquals(0, service.getAllStudents().size());
+	}
+	@Test
+	public void testCreateNullEmailStudent() {
+		String error = null;
+		try {
+			service.createStudent("first_name", "last_name", 260112233, null);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
 
-    }
-    
-    @Test
-    public void testCreateTask() {
-        @SuppressWarnings("deprecation")
-		Date dueDate =new Date(2019, 1, 1); 
-        CoopCourse c = service.createCoopCourse("ECSE302", 1);
-        CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
-        Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
-        Employer e = service.createEmployer("Facebook", "fb@email.com");
-        StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, e, cco);
+		assertEquals("Your student details are incomplete!", error);
 
-        service.createTask("Some description", dueDate , TaskStatus.COMPLETED, "1234", se);
+		assertEquals(0, service.getAllStudents().size());
+	}
 
-        Task t = taskRepository.findTaskByTaskID("1234");
+	/*--- EMPLOYER TESTS ---*/
+	@Test
+	public void testCreateEmployer() {
+		try {
+			service.createEmployer("Google", "google@gmail.com");
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
 
-        assertEquals("Some description", t.getDescription());
-        assertEquals(dueDate, t.getDueDate());
-        assertEquals(TaskStatus.COMPLETED, t.getTaskStatus());
-        assertEquals("1234", t.getTaskID());
-    }
-    
-    @Test
-    public void testCreateDocument() {
-        @SuppressWarnings("deprecation")
-		Date dueDate =new Date(2019, 1, 1); 
-        CoopCourse c = service.createCoopCourse("ECSE303", 1);
-        CoopCourseOffering cco = service.createCoopCourseOffering(2017, Term.FALL, true, c);
-        Student s = service.createStudent("f_name", "l_name", 260654322, "test@mail.com");
-        Employer e = service.createEmployer("Facebook", "fb@email.ca");
-        StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, e, cco);
-        Task t = service.createTask("Some description", dueDate , TaskStatus.COMPLETED, "1235", se);
+		Employer e = service.getEmployer("google@gmail.com");
 
-        service.createDocument("doc name", "http://test-url.this/is/just/for/testing", t);
+		assertEquals("Google", e.getName());
+		assertEquals("google@gmail.com", e.getEmail());
 
-        Document d = documentRepository.findDocumentByUrl("http://test-url.this/is/just/for/testing");
+		assertEquals(1, service.getAllEmployers().size());
+	}
 
-        assertEquals("doc name", d.getName());
-        assertEquals("http://test-url.this/is/just/for/testing", d.getUrl());
-    }
-	
+	@Test
+	public void testCreateNullNameEmployer() {
+		String error = null;
+		try {
+			service.createEmployer(null, "google@gmail.com");
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+
+		assertEquals("Your employer details are incomplete!", error);
+
+		assertEquals(0, service.getAllEmployers().size());
+	}
+
+	/*--- CO-OP COURSE TESTS ---*/
+	@Test
+	public void testCreateCoopCourse() {
+		try {
+			service.createCoopCourse("ECSE300", 1);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		CoopCourse c = coopCourseRepository.findCoopCourseByCourseCode("ECSE300");
+
+		assertEquals("ECSE300", c.getCourseCode());
+		assertEquals((Integer) 1, c.getCoopTerm());
+
+		assertEquals(1, service.getAllCoopCourses().size());
+	}
+
+	@Test
+	public void testCreateNullCodeCoopCourse() {
+		String error = null;
+		try {
+			service.createCoopCourse(null, 1);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		assertEquals("Your course details are incomplete!", error);
+
+		assertEquals(0, service.getAllCoopCourses().size());
+	}
+
+	/*--- CO-OP COURSE OFEERING TESTS ---*/
+	@Test
+	public void testCreateCoopCourseOffering() {
+		CoopCourse c = service.createCoopCourse("ECSE301", 1);
+
+		try {
+			service.createCoopCourseOffering(2018, Term.WINTER, true, c);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+
+		CoopCourseOffering cco = coopCourseOfferingRepository.findCoopCourseOfferingByOfferID("ECSE301-W18");
+
+		assertEquals((Integer) 2018, cco.getYear());
+		assertEquals(Term.WINTER, cco.getTerm());
+		assertEquals(true, cco.getActive());
+		assertEquals("ECSE301-W18", cco.getOfferID());
+
+		assertEquals(1, service.getAllCoopCourseOfferings().size());
+	}
+
+	@Test
+	public void testCreateNullYearCoopCourseOffering() {
+		CoopCourse c = service.createCoopCourse("ECSE301", 1);
+		String error = null;
+		try {
+			service.createCoopCourseOffering(null, Term.WINTER, true, c);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		assertEquals("Your course offering details are incomplete!", error);
+
+		assertEquals(0, service.getAllCoopCourseOfferings().size());
+	}
+
+	/*--- STUDENT ENROLLMENT TESTS ---*/
+	@Test
+	public void testCreateStudentEnrollment() {
+		CoopCourse c = service.createCoopCourse("ECSE302", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.com");
+
+		try {
+			service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		StudentEnrollment se = studentEnrollmentRepository.findStudentEnrollmentByEnrollmentID("260654321-ECSE302-F19");
+
+		assertEquals(true, se.getActive());
+		assertEquals("260654321-ECSE302-F19", se.getEnrollmentID());
+		assertEquals(CourseStatus.PASSED, se.getStatus());
+
+		assertEquals(1, service.getAllStudentEnrollments().size());
+
+	}
+
+	@Test
+	public void testCreateNullStatusStudentEnrollment() {
+		String error = null;
+		CoopCourse c = service.createCoopCourse("ECSE302", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.com");
+
+		try {
+			service.createStudentEnrollment(null, CourseStatus.PASSED, s, emp, cco);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+
+		assertEquals("Your student enrollment details are incomplete!", error);
+
+		assertEquals(0, service.getAllStudentEnrollments().size());
+
+	}
+
+	/*--- TASK TESTS ---*/
+	@Test
+	public void testCreateTask() {
+		@SuppressWarnings("deprecation")
+		Date dueDate = new Date(2019, 1, 1);
+		CoopCourse c = service.createCoopCourse("ECSE302", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.com");
+		StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
+
+		try {
+			service.createTask("Some description", dueDate, TaskStatus.COMPLETED, "1234", se);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		Task t = taskRepository.findTaskByTaskID("1234");
+
+		assertEquals("Some description", t.getDescription());
+		assertEquals(dueDate, t.getDueDate());
+		assertEquals(TaskStatus.COMPLETED, t.getTaskStatus());
+		assertEquals("1234", t.getTaskID());
+
+		assertEquals(1, service.getAllTasks().size());
+	}
+
+	@Test
+	public void testCreateNullDescriptionTask() {
+		String error = null;
+		@SuppressWarnings("deprecation")
+		Date dueDate = new Date(2019, 1, 1);
+		CoopCourse c = service.createCoopCourse("ECSE302", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2019, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654321, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.com");
+		StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
+
+		try {
+			service.createTask(null, dueDate, TaskStatus.COMPLETED, "1234", se);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+
+		assertEquals("Your task details are incomplete!", error);
+
+		assertEquals(0, service.getAllTasks().size());
+
+	}
+
+	/*--- DOCUMENT TESTS ---*/
+	@Test
+	public void testCreateDocument() {
+		@SuppressWarnings("deprecation")
+		Date dueDate = new Date(2019, 1, 1);
+		CoopCourse c = service.createCoopCourse("ECSE303", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2017, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654322, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.ca");
+		StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
+		Task t = service.createTask("Some description", dueDate, TaskStatus.COMPLETED, "1235", se);
+		try {
+			service.createDocument("doc name", "http://test-url.this/is/just/for/testing", t);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		Document d = documentRepository.findDocumentByUrl("http://test-url.this/is/just/for/testing");
+
+		assertEquals("doc name", d.getName());
+		assertEquals("http://test-url.this/is/just/for/testing", d.getUrl());
+
+		assertEquals(1, service.getAllDocuments().size());
+	}
+
+	@Test
+	public void testCreateNullNameDocument() {
+		String error = null;
+		@SuppressWarnings("deprecation")
+		Date dueDate = new Date(2019, 1, 1);
+		CoopCourse c = service.createCoopCourse("ECSE303", 1);
+		CoopCourseOffering cco = service.createCoopCourseOffering(2017, Term.FALL, true, c);
+		Student s = service.createStudent("f_name", "l_name", 260654322, "test@mail.com");
+		Employer emp = service.createEmployer("Facebook", "fb@email.ca");
+		StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
+		Task t = service.createTask("Some description", dueDate, TaskStatus.COMPLETED, "1235", se);
+		try {
+			service.createDocument(null, "http://test-url.this/is/just/for/testing", t);
+		} catch (IllegalArgumentException e) {
+			error = e.getMessage();
+		}
+		
+		assertEquals("Your document details are incomplete!", error);
+
+		assertEquals(0, service.getAllDocuments().size());
+
+	}
+
 }
