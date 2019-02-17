0e8d40eaf7eb8ce6e138f1157a6b9bc80439a1d8
unknown <remi.carriere@mail.mcgill.ca>
2019-02-17 13:09:39 -0500

    removed unused test and method

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestDocument.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestDocument.java
@@ -56,43 +56,4 @@ public class TestDocument {
 		assertEquals(0, service.getAllDocuments().size());
 	}
 
-	/* Really messy, but I think it works? */
-//	@Test
-//	public void testReplaceDocument() {
-//		@SuppressWarnings("deprecation")
-//		Date dueDate = new Date(2019, 1, 1);
-//		CoopCourse c = service.createCoopCourse("ECSE303", 1);
-//		CoopCourseOffering cco = service.createCoopCourseOffering(2017, Term.FALL, true, c);
-//		Student s = service.createStudent("f_name", "l_name", 260654322, "test@mail.com");
-//		Employer emp = service.createEmployer("Facebook", "fb@email.ca");
-//		StudentEnrollment se = service.createStudentEnrollment(true, CourseStatus.PASSED, s, emp, cco);
-//		Task t = service.createTask("Some description", dueDate, TaskStatus.COMPLETED, se);
-//		long taskID = t.getTaskID();
-//		try {
-//			service.createDocument("doc name", "http://test-url.this/is/just/for/testing", t);
-//		} catch (IllegalArgumentException e) {
-//			fail();
-//		}
-//		Document oldDoc = service.getDocument("http://test-url.this/is/just/for/testing");
-//		// check that old doc was persisted
-//		assertEquals("doc name", oldDoc.getName());
-//		assertEquals("http://test-url.this/is/just/for/testing", oldDoc.getUrl());
-//		assertEquals(t.getTaskID(), oldDoc.getTask().getTaskID());
-//
-//		// create a new doc
-//		Document newDoc = service.createDocument("doc name", "http://replacement/doc", t);
-//
-//		// delete the old doc and persist the new doc
-//		service.replaceTaskDocument(t.getTaskID(), newDoc, oldDoc.getUrl());
-//
-//		// find the new doc assuming it should be associated with the same task ID
-//		t = service.getTask(taskID);
-//		newDoc = service.getDocument("http://replacement/doc");
-//
-//		assertEquals(t.getTaskID(), newDoc.getTask().getTaskID()); // old task id same as task id associated with new
-//																	// document
-//		assertEquals(1, service.getAllDocuments().size()); // there should only be one doc since the other is deleted in
-//															// the transaction
-//	}
-
 }
