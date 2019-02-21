1b43560cc1aa1e4f87e8282a0562ad803dc529fb
Torres-Ma <boyang.ma@mail.mcgill.ca>
2019-02-21 13:42:11 -0500

    Added Dto

---
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/controller/CooperatorRestController.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.cooperator.controller;
+
+public class CooperatorRestController {
+
+}
---
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/dto/StudentDto.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.cooperator.dto;
+
+public class StudentDto {
+
+}
