0bced59b49e0dd8133368a90e2257071980644dd
Torres-Ma <boyang.ma@mail.mcgill.ca>
2019-02-22 16:08:30 -0500

    Added application test class - empty

---
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/application/cooperatorApplicatonTests.java
@@ -0,0 +1,18 @@
+package ca.mcgill.ecse321.cooperator.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class cooperatorApplicatonTests {
+	
+	@Test
+    public void contextLoads() {
+
+    }
+
+}
