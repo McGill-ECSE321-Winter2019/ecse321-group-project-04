1ebc1021384c3270e2654098257a404c853f6897
Torres-Ma <boyang.ma@mail.mcgill.ca>
2019-02-22 12:35:53 -0500

    Added Document Dto

--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/dto/DocumentDto.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/dto/DocumentDto.java
@@ -1,5 +1,34 @@
 package ca.mcgill.ecse321.cooperator.dto;
 
+import ca.mcgill.ecse321.cooperator.model.Task;
+
 public class DocumentDto {
+	private String name;
+    private String url;
+    private Task task;
+    
+    public DocumentDto(){ 
+    } 
+    
+    public DocumentDto(String name, Task task) { 
+    	this.name = name; 
+    	this.task = task; 
+    }
+    
+    public String getName() { 
+    	return this.name; 
+    }
+  
+    public Task getTask() {
+    	return this.task; 
+    }
+    
+    public void setUrl(String value) { 
+    	this.url = value; 
+    }
+    
+    public String getUrl() { 
+    	return this.url; 
+    }
 
 }
--- Backend/src/main/java/ca/mcgill/ecse321/cooperator/dto/TaskDto.java
+++ Backend/src/main/java/ca/mcgill/ecse321/cooperator/dto/TaskDto.java
@@ -1,5 +1,29 @@
 package ca.mcgill.ecse321.cooperator.dto;
 
+import java.sql.Date;
+import java.util.Set;
+
+import ca.mcgill.ecse321.cooperator.model.Document;
+import ca.mcgill.ecse321.cooperator.model.TaskStatus;
+
 public class TaskDto {
+	
+	private TaskStatus taskStatus;
+	
+    private long taskID;
+    private String description;
+    private Date dueDate;
+    private Set<Document> document;
+    
+    public TaskDto () { 
+    }
+    
+    public TaskDto( String description, Date dueDate) { 
+    	this.description = description; 
+    	this.dueDate = dueDate; 
+    }
+    
+    
+    
 
 }
