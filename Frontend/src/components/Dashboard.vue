fbb1c26c1b169bac641c51c0d3280368d8bfa0ee
angel6497 <angel.ortizregules@mail.mcgill.ca>
2019-03-20 15:10:59 -0400

    Added basic dashboard, and removed Vue logo

---
+++ Frontend/src/components/Dashboard.vue
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<template>
+  <html lang="en">
+
+  <head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  </head>
+
+  <body>
+
+    <div class="container-fluid" id="top-container">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>Co-Op-Erator</h2>
+          </div>
+          <div class="col-sm-4">
+            <a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <nav class="navbar navbar">
+      <div class="container-fluid">
+        <div class="navbar-header">
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav nav-tabs">
+            <li class="active"><a href="#">Active Courses</a></li>
+            <li><a href="#">Archieved Courses</a></li>
+            <li><button type="button" class="btn btn-success" href="#">Register Course</button></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-danger">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-success">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+      </div>
+    </div><br>
+    <br>
+
+    <footer class="container-fluid text-center">
+      <p>Online Store Copyright</p>
+      <form class="form-inline">Get deals:
+        <input type="email" class="form-control" size="50" placeholder="Email Address">
+        <button type="button" class="btn btn-danger">Sign Up</button>
+      </form>
+    </footer>
+
+  </body>
+
+  </html>
+</template>
+
+<style>
+  /* Remove the navbar's default rounded borders and increase the bottom margin */
+  .navbar {
+    margin-bottom: 50px;
+    border-radius: 0;
+  }
+
+  /* Remove the jumbotron's default bottom margin */
+  #top-container {
+    margin-bottom: 0;
+    background-color: #333335;
+    color: #ffffff;
+  }
+
+  #top-container h2 {
+    text-align: left;
+    margin-top: 30px;
+    margin-bottom: 15px;
+  }
+
+  #top-container a {
+    display: inline-block;
+    margin-top: 35px;
+  }
+
+  nav {
+    margin-top: 15px;
+  }
+
+  /* Add a gray background color and some padding to the footer */
+  footer {
+    background-color: #f2f2f2;
+    padding: 25px;
+  }
+</style>
+
+<script>
+  export default {
+    data: () => ({}),
+    mounted() {
+      let s1 = document.createElement('script')
+      s1.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js')
+      document.head.appendChild(s1)
+      let s2 = document.createElement('script')
+      s2.setAttribute('src', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js')
+      document.head.appendChild(s2)
+    },
+    methods: {}
+  }
+</script>
