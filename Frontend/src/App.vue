0a486b29c572e591902f99ba6c042a589aa91d1a
fangnx <fangnaxin@gmail.com>
2019-03-20 14:37:10 -0400

    Added a navigation bar & set up url routing

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,23 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <a class="navbar-brand" href="/">COOPERATOR</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" aria>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/Registration/">Registration</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/Report/">Report</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/Progress/">Tracking Progress</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
   </div>
-</template>
+</template> 
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, sans-serif;
+    font-size: 18px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+  }
+  .navbar-nav > li {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .navbar-brand {
+    font-family: panti;
+    padding-right: 20px;  
+  }
+
 </style>
---
+++ Frontend/src/components/Report.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: 'Report'
+}
+</script>
+
+<style>
+</style>    
