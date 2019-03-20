bee27ae4ab66f9b98d52fdecfa65ffb0dff02fa4
fangnx <fangnaxin@gmail.com>
2019-03-20 15:13:49 -0400

    Fixed routing when deployed to Heroku

--- Frontend/server.js
+++ Frontend/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(require('connect-history-api-fallback')())
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
