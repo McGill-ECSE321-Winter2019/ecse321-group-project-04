78913a5fab5be7eef85218aa641f5095d2f495ff
angel6497 <angel.ortizregules@mail.mcgill.ca>
2019-03-20 15:29:17 -0400

    Revert "Added a navigation bar & set up url routing"
    This reverts commit 0a486b29c572e591902f99ba6c042a589aa91d1a.

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,34 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Registration from '@/components/Registration'
-import Report from '@/components/Report'
-import Progress from '@/components/progress'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
-      path: '/hello',
+      path: '/',
       name: 'Hello',
       component: Hello
-    },
-    {
-      path: '/Registration',
-      name: 'Registration',
-      component: Registration
-    },
-    {
-      path: '/Report',
-      name: Report,
-      component: Report
-    },
-    {
-      path: 'Progress',
-      name: Progress,
-      component: Progress
     }
   ]
 })
