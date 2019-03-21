2405dca45703670107403f9fda511a73d013b765
angel6497 <angel.ortizregules@mail.mcgill.ca>
2019-03-21 15:36:11 -0400

    Test for cross origin

--- Frontend/src/components/Dashboard.vue
+++ Frontend/src/components/Dashboard.vue
@@ -114,8 +114,8 @@
   import axios from 'axios'
   var config = require('../../config')
 
-  var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-  var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+  var frontendUrl = 'https://' + config.dev.host + ':' + config.dev.port
+  var backendUrl = 'https://' + config.dev.backendHost + ':' + config.dev.backendPort
 
   var AXIOS = axios.create({
     baseURL: backendUrl,
