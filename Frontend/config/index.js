aaacdf9df4219d77dcd0659777d46ded8de1dd62
angel6497 <angel.ortizregules@mail.mcgill.ca>
2019-03-21 21:25:54 -0400

    Improved CourseInfo component

--- Frontend/config/index.js
+++ Frontend/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report,
-    host: 'cooperator-frontend-0000.herokuapp.com/',
+    host: 'cooperator-frontend-0000.herokuapp.com',
     port: 443,
     backendHost: 'cooperator-backend-0000.herokuapp.com/',
     backendPort: 443,
--- Frontend/src/components/CourseInfo.vue
+++ Frontend/src/components/CourseInfo.vue
@@ -16,7 +16,7 @@
       <div class="container text-center">
         <div class="row">
           <div class="col-sm-8">
-            <h2>Co-Op-Erator {{this.$route.params.studentID}} {{student != null?student.firstName: '-'}}</h2>
+            <h2>Co-Op-Erator</h2>
           </div>
           <div class="col-sm-4">
             <div class="btn-group">
@@ -39,29 +39,29 @@
       <div class="container text-center">
         <div class="row">
           <div class="col-sm-12">
-
             <!-- The course name here on line 36 has to be changed to the real one that link to the dashboard -->
+
             <h2>Winter 2019 - FACC 201</h2>
           </div>
         </div>
       </div>
     </div>
 
-    <nav class="navbar navbar">
-      <div class="container-fluid">
-        <div class="navbar-header">
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav nav-tabs">
-            <li v-for="tab in tabs" :class="selectedTab === tab ? 'active' : ''" @click="selectedTab = tab">
-              <a>
-                <font size="4">{{ tab }}</font>
-              </a>
-            </li>
-          </ul>
+    <div class="container text-center">
+      <div class="container-fluid" id="nav-bar">
+        <div class="row">
+          <div class="col-sm-12">
+            <ul class="nav nav-tabs">
+              <li v-for="tab in tabs" :class="selectedTab === tab ? 'active' : ''" @click="selectedTab = tab">
+                <a>
+                  <font size="4">{{ tab }}</font>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
     <br>
 
     <div v-if="selectedTab === 'Overview'" class="container" id="overview-container">
@@ -93,32 +93,32 @@
     </div>
 
     <div v-if="selectedTab === 'Tasks'" class="container">
-      <table class="table table-bordered">
+      <table class="table table-striped table-bordered">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col" style="text-align:center; vertical-align:middle">
+              <h4>Task</h4>
+            </th>
+            <th scope="col" style="text-align:center; vertical-align:middle">
+              <h4>Completion Status</h4>
+            </th>
+            <th scope="col" style="text-align:center; vertical-align:middle">
+              <h4>Due Date</h4>
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
+          <tr v-for="task in tasks">
+            <td>
+              <h4><a href="#" @click="goToTask(task)">{{task.name}}</a></h4>
+              <h5 style="color:gray">{{task.description}}</h5>
+            </td>
+            <td style="text-align:center; vertical-align:middle">
+              <h5 :style="task.taskStatus == 'COMPLETED' ? 'color:green' : 'color:'">{{taskStatusDisplay[task.taskStatus]}}</h5>
+            </td>
+            <td style="text-align:center; vertical-align:middle">
+              {{ displayDate(task.dueDate) }}
+            </td>
           </tr>
         </tbody>
       </table>
@@ -159,7 +159,6 @@
     text-align: left;
     margin-top: 30px;
     margin-bottom: 15px;
-
   }
 
   nav {
@@ -168,10 +167,62 @@
 </style>
 
 <script>
-
+  import Router from 'vue-router'
   import axios from 'axios'
   var config = require('../../config')
 
+  /* Date handling */
+  var months = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+  }
+
+  function parseDate(dateString) {
+    var vals = dateString.split('-')
+    return {
+      year: vals[0],
+      month: vals[1],
+      day: vals[2]
+    }
+  }
+
+  function compareDates(d1, d2) {
+    // Compare the years
+    if (d1.year > d2.year) {
+      return 1
+    } else if (d1.year < d2.year) {
+      return -1
+    }
+
+    // If equal compare the months
+    if (d1.month > d2.month) {
+      return 1
+    } else if (d1.month < d2.month) {
+      return -1
+    }
+
+    // If equal compare the days
+    if (d1.day > d2.day) {
+      return 1
+    } else if (d1.day < d2.day) {
+      return -1
+    }
+
+    // Else they are the same
+    return 0
+  }
+
+  /* AXIOS object configuration */
   var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
   var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
@@ -188,6 +239,11 @@
       return {
         tabs: ['Overview', 'Tasks', 'Information'],
         selectedTab: 'Overview',
+        taskStatusDisplay: {
+          COMPLETED: 'Completed',
+          INCOMPLETE: 'Incomplete',
+          LATE_SUBMITTED: 'Completed Late'
+        },
         courseOffering: {
           "term": "WINTER",
           "year": 2019,
@@ -296,16 +352,43 @@
       }
     },
     created() {
-      AXIOS.get(`/students/` + this.$route.params.studentID)
-        .then(response => {
-          this.student = response.data
-        })
-        .catch(e => {
-          var errorMsg = e.message
-          console.log(errorMsg)
-          this.errorPerson = errorMsg
-        })
+      // Convert all the dates to date objects
+      for (var task in this.tasks) {
+        task.dueDate = parseDate(task.dueDate)
+      }
     },
-    methods: {}
+    methods: {
+      goToTask: function(task) {
+        var taskURL = task._links.task.href.split('/')
+        var taskID = taskURL[taskURL.length - 1]
+        console.log(taskID)
+        this.$router.push({
+          name: 'Taskview',
+          params: {
+            id: taskID
+          }
+        })
+      },
+      displayDate: function(d) {
+        d = parseDate(d)
+        var display = months[parseInt(d.month)] + ' ' + parseInt(d.day)
+        switch (d.day % 10) {
+          case 1:
+            display += 'st'
+            break
+          case 2:
+            display += 'nd'
+            break
+          case 3:
+            display += 'rd'
+            break
+          default:
+            display += 'th'
+            break
+        }
+        display += ', ' + d.year
+        return display
+      }
+    }
   }
 </script>
