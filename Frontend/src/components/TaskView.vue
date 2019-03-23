12316284542fc6c9be70f6f5ddab668d6b8e85db
fangnx <fangnaxin@gmail.com>
2019-03-23 17:42:45 -0400

    Updated Modal and TaskView UI

--- Frontend/src/components/TaskView.vue
+++ Frontend/src/components/TaskView.vue
@@ -7,7 +7,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css"
+      integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
   </head>
 
   <body>
@@ -21,7 +22,9 @@
                 <div class="col-sm-3">
                   <div class="container  text-left">
                     <div @click="goToDashboard">
-                      <img src="https://user-images.githubusercontent.com/35735496/54735369-2f1d7b80-4b7c-11e9-93a2-505866f8ec69.png" width="300" height="100">
+                      <img
+                        src="https://user-images.githubusercontent.com/35735496/54735369-2f1d7b80-4b7c-11e9-93a2-505866f8ec69.png"
+                        width="300" height="100">
                     </div>
                   </div>
                 </div>
@@ -66,153 +69,155 @@
     </div>
     <br>
 
-    <div class="container-fluid">
-      <div class="container text-left" id="information">
-        <div class="row">
-          <div class="col-sm-2 sidenav"></div>
-          <div class="col-sm-8">
-            <h3>
-              <font>Task Description</font>
-            </h3>
-            <h4>Reports are due on the Friday of the week following the lab session.Only one report per group of two students
-              should be submitted. Make sure to put the name and student numbers of both group members on the report,
-              otherwise grades will not be assigned. Students are responsible for all content in the reports.</h4>
-            <h3>
-              <font>Due date</font>
-            </h3>
-            <h4> 2019-03-21 </h4>
-            <br><br>
-          </div>
-          <div class="col-sm-2 sidenav"></div>
+    <div class="task-card">
+
+      <div class="row">
+        <div class="col-sm-2 sidenav"></div>
+        <div class="col-sm-8">
+          <h3>
+            Task Description
+          </h3>
+          <h4 class="task-description">
+            Reports are due on the Friday of the week following the lab session. Only one report per group of two
+            students should be submitted. Make sure to put the name and student numbers of both group members on the
+            report,
+            otherwise grades will not be assigned. Students are responsible for all content in the reports.</h4>
+          <h3>
+            Due date
+          </h3>
+          <h4 class="task-description"> 2019-03-21 </h4>
+          <br><br>
         </div>
+        <div class="col-sm-2 sidenav"></div>
       </div>
     </div>
 
-    <div class="container text-center">
-      <div class="container-fluid" id="nav-bar">
+    <br />
+    <br />
+
+    <div class="container-fluid" id="task-submission">
+
+      <div class="row">
+        <div class="col-sm-1 sidenav"></div>
+        <div class="col-sm-10">
+          <ul class="nav nav-tabs">
+            <li v-for="tab in tabs" :class="selectedTab === tab ? 'active' : ''" @click="selectedTab = tab">
+              <a>
+                <font size="4">{{ tab }}</font>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-sm-1 sidenav"></div>
+      </div>
+
+      <br>
+
+      <div v-if="selectedTab === 'Submit Document'" class="container-fluid" id="Submit Document-container">
         <div class="row">
           <div class="col-sm-1 sidenav"></div>
           <div class="col-sm-10">
-            <ul class="nav nav-tabs">
-              <li v-for="tab in tabs" :class="selectedTab === tab ? 'active' : ''" @click="selectedTab = tab">
-                <a>
-                  <font size="4">{{ tab }}</font>
-                </a>
-              </li>
-            </ul>
+            <div class="col-sm-6">
+              <div class="card border-inverse mb-3">
+                <div class="card-body">
+                  <h3 class="card-title" style="margin-top:10px; margin-bottom:20px;">Attach Document(s)</h3>
+
+                  <button @click="showModal=true" type="button" class="btn btn-primary">
+                    Add File(s)
+                  </button>
+
+                  <!-- Modal -->
+                  <div v-if="showModal">
+                    <transition name="modal">
+                      <div class="modal-mask">
+
+                        <div class="modal-wrapper" @click="showModal=false">
+                          <div class="modal-container" @click.stop>
+
+                            <div class="modal-header">
+                              <slot name="header">
+                                File Name
+                              </slot>
+                            </div>
+
+                            <div class="modal-body">
+                              <slot name="body">
+                                <div id="upload">
+                                  <div v-if="!file">
+                                    <input type="file" @change="onFileChange">
+                                  </div>
+                                  <div v-else>
+                                    <button @click="removeFile">Remove File</button>
+                                    <span>{{file}}</span>
+                                  </div>
+                                </div>
+                              </slot>
+                            </div>
+
+                            <div class="modal-footer">
+                              <slot name="footer">
+                                <button class="btn btn-primary btn-sm" @click="showModal=false">
+                                  OK
+                                </button>
+                              </slot>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </transition>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <div class="card border-inverse mb-3">
+                <div class="card-body">
+                  <h3 class="card-title" style="margin-top:10px; margin-bottom:30px;">Information</h3>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-sm-1 sidenav"></div>
         </div>
       </div>
-    </div>
-    <br>
 
-    <div v-if="selectedTab === 'Submit Document'" class="container" id="Submit Document-container">
-      <div class="row">
-        <div class="col-sm-1 sidenav"></div>
-        <div class="col-sm-10">
-          <div class="col-sm-6">
-            <div class="card border-inverse mb-3">
-              <div class="card-body">
-                <h3 class="card-title" style="margin-top:10px; margin-bottom:20px;">Attach Document(s)</h3>
-
-                <button @click="showModal=true" type="button" class="btn btn-primary">
-                  Add File(s)
-                </button>
-
-                <!-- Modal -->
-                <div v-if="showModal">
-                  <transition name="modal">
-                    <div class="modal-mask">
-
-                      <div class="modal-wrapper">
-                        <div class="modal-container">
-
-                          <div class="modal-header">
-                            <slot name="header">
-                              File Name
-                            </slot>
-                          </div>
-
-                          <div class="modal-body">
-                            <slot name="body">
-                              <div id="upload">
-                                <div v-if="!file">
-                                  <input type="file" @change="onFileChange">
-                                </div>
-                                <div v-else>
-                                  <button @click="removeFile">Remove File</button>
-                                  <span>{{file}}</span>
-                                </div>
-                              </div>
-                            </slot>
-                          </div>
-
-                          <div class="modal-footer">
-                            <slot name="footer">
-                              <button class="btn btn-primary btn-sm" @click="showModal=false">
-                                OK
-                              </button>
-                            </slot>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </transition>
-
-                </div>
-
-              </div>
-            </div>
+      <div v-if="selectedTab === 'Submission History'" class="container-fluid">
+        <div class="row">
+          <div class="col-sm-1 sidenav"></div>
+          <div class="col-sm-10">
+            <table class="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col" style="text-align:center; vertical-align:middle">
+                    <h4>Submission(s)</h4>
+                  </th>
+                  <th scope="col" style="text-align:center; vertical-align:middle">
+                    <h4>Date submitted</h4>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="submisson in submissions">
+                  <td style="text-align:center; vertical-align:middle">
+                    <!-- the name of file submitted should be displayed -->
+                    <h5>allo</h5>
+                  </td>
+                  <td style="text-align:center; vertical-align:middle">
+                    <h5>{{ displayDate(submisson.dueDate) }}</h5>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+          <div class="col-sm-1 sidenav"></div>
 
-          <div class="col-sm-6">
-            <div class="card border-inverse mb-3">
-              <div class="card-body">
-                <h3 class="card-title" style="margin-top:10px; margin-bottom:30px;">Information</h3>
-              </div>
-            </div>
-          </div>
         </div>
-        <div class="col-sm-1 sidenav"></div>
-      </div>
-    </div>
 
-    <div v-if="selectedTab === 'Submission History'" class="container">
-      <div class="row">
-        <div class="col-sm-1 sidenav"></div>
-        <div class="col-sm-10">
-          <table class="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th scope="col" style="text-align:center; vertical-align:middle">
-                  <h4>Submission(s)</h4>
-                </th>
-                <th scope="col" style="text-align:center; vertical-align:middle">
-                  <h4>Date submitted</h4>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="submisson in submissions">
-                <td style="text-align:center; vertical-align:middle">
-                  <!-- the name of file submitted should be displayed -->
-                  <h5>allo</h5>
-                </td>
-                <td style="text-align:center; vertical-align:middle">
-                  <h5>{{ displayDate(submisson.dueDate) }}</h5>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="col-sm-1 sidenav"></div>
-      </div>
-    </div>
-
-    <br>
-    <br>
-
+        <br>
+        <br>
 
   </body>
 
@@ -261,6 +266,23 @@
     min-width: 100%;
   }
 
+  .task-card {
+    box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.2);
+    width: 66%;
+    height: auto;
+    margin: auto;
+    text-align: left;
+  }
+
+  .task-description {
+    font-style: italic;
+    font-weight: 400;
+  }
+
+  #task-submission {
+    width: 80%;
+  }
+
   .modal-mask {
     position: fixed;
     z-index: 9998;
@@ -292,7 +314,6 @@
   .modal-header {
     margin-top: 0;
     font-size: 24px;
-    color: #42b983;
   }
 
   .modal-body {
@@ -325,6 +346,7 @@
     -webkit-transform: scale(1.1);
     transform: scale(1.1);
   }
+
 </style>
 
 <script>
@@ -443,10 +465,10 @@
           return;
         this.file = files[0].name
       },
-      removeFile: function(e) {
+      removeFile: function (e) {
         this.file = '';
       },
-      displayDate: function(d) {
+      displayDate: function (d) {
         d = parseDate(d)
         var display = months[parseInt(d.month)] + ' ' + parseInt(d.day)
         switch (d.day % 10) {
@@ -466,7 +488,7 @@
         display += ', ' + d.year
         return display
       },
-      goToDashboard: function() {
+      goToDashboard: function () {
         this.$router.push({
           name: 'Dashboard',
           params: {
@@ -474,12 +496,12 @@
           }
         })
       },
-      goToLogin: function() {
+      goToLogin: function () {
         this.$router.push({
           name: 'Login',
         })
       },
-      goToAccount: function() {
+      goToAccount: function () {
         this.$router.push({
           name: 'StudentInformation',
           params: {
@@ -489,4 +511,5 @@
       }
     }
   }
+
 </script>
