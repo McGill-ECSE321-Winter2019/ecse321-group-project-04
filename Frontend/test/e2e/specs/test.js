dbd33fbcee4df31147bd595f1049e7b217413259
fangnx <fangnaxin@gmail.com>
2019-03-24 13:12:29 -0400

    Added initial web e2e test cases

--- Frontend/test/e2e/specs/test.js
+++ Frontend/test/e2e/specs/test.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
+  'Login': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
@@ -11,9 +11,24 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.title('frontend')
+      .waitForElementVisible('body', 2000)
+      .assert.urlContains('login')
+
+      // test student log-in
+      .assert.visible('.form-group')
+      .assert.visible('#login')
+      .waitForElementVisible('input[id="usr"]', 2000)
+      .click('button[id="login"]')
+      .assert.containsText('#demo', 'Please Enter Correct Sudent ID')
+      .pause(500)
+      .setValue('input[id=usr]', '260123456')
+      .pause(500)
+      .click('button[id="login"]')
+      .waitForElementVisible('#nav-bar', 2000)
+      .assert.urlContains('dashboard')
+
       .end()
-  }
+  },
+
 }
