9a70758a773975f382513da7ad1d90c7e029c8bf
angel6497 <angel.ortizregules@mail.mcgill.ca>
2019-03-30 15:46:12 -0400

    Added hover effect for logo in student information

--- Frontend/src/components/StudentInformation.vue
+++ Frontend/src/components/StudentInformation.vue
@@ -16,7 +16,7 @@
         <div class="row">
           <div class="col-sm-6">
             <div class="container  text-left">
-              <div @click="goToDashboard" style="display: inline-block;">
+              <div @click="goToDashboard" style="display: inline-block;" id="img-container">
                 <img src="https://user-images.githubusercontent.com/35735496/54735369-2f1d7b80-4b7c-11e9-93a2-505866f8ec69.png" width="300" height="100">
               </div>
             </div>
@@ -69,6 +69,10 @@
 </template>
 
 <style>
+  #img-container:hover img {
+    opacity: 0.8;
+  }
+
   .panel {
     min-height: 80%;
     min-width: 100%;
