4b418122cc25c84afb6508409fd4be66b0aa98ef
fangnx <fangnaxin@gmail.com>
2019-03-30 17:07:41 -0400

    Removed unused imported stylesheet

--- Frontend/src/components/StudentInformation.vue
+++ Frontend/src/components/StudentInformation.vue
@@ -7,8 +7,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   </head>
 
   <body>
